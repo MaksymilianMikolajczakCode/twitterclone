2032263cfec2ca5cbd6abd7dacfd7648d8f1af3b
MaksymilianMikolajczakCode <maksymilianmikolajczakcode@gmail.com>
2023-08-16 04:36:23 +0100

    progressx2

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -37,7 +37,7 @@ const NAVIGATION_ITEMS = [
 
 const LeftSidebar = () => {
   return (
-    <section className='fixed w-[275px] items-stretch flex flex-col h-screen px-6'>
+    <section className='sticky top-0 w-[25%] items-stretch flex flex-col h-screen'>
     <div className='flex flex-col items-stretch h-screen space-y-4 my-4'>
     {NAVIGATION_ITEMS.map((item)=>(
       <Link className='hover:bg-white/10 rounded-3xl text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 p-2' 
@@ -53,7 +53,7 @@ const LeftSidebar = () => {
       Tweet
     </button>
     </div>
-    <button className='rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200 w-full justify-between'>
+    <button className='rounded-full flex items-center space-x-2 bg-transparent p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200 w-full justify-between'>
       <div className='flex items-center space-x-2'>
       <div className='rounded-full bg-slate-400 w-10 h-10'></div>
         <div className='text-left text-sm'>
