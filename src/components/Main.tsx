f21d4fcbaaaa1646eab3f7024cbdafb73746771c
MaksymilianMikolajczakCode <maksymilianmikolajczakcode@gmail.com>
2023-08-25 03:26:18 +0100

    progressx5

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,30 +1,27 @@
-'use client'
 
 import { BsChat, BsDot, BsThreeDots } from "react-icons/bs"
 import { AiOutlineHeart, AiOutlineRetweet } from "react-icons/ai"
 import { IoStatsChart, IoShareOutline } from "react-icons/io5"
+import ComposeTweet from "./serverComponents/composeTweet"
 
 const Main = () => {
+
+  // const supabaseClient = createServerComponentSupabaseClient({
+  //   cookies,
+  //   headers,
+  // });
+
+  // const { data: userData, error: userError } =
+  //   await supabaseClient.auth.getUser();
+
+  // const res = await getTweets({ currentUserID: userData.user?.id });
   return (
     <main className="flex w-full h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
     <h1 className="text-xl font-bold my-4 p-6 backdrop-blur bg-black/10 sticky top-0">Home</h1>
     <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-stretch py-6 space-x-2 border-gray-600 relative">
-      <div className="w-10 h-10 bg-slate-400 w-full rounded-full"></div>
-      <div className="flex flex-col w-full h-full">
-        <input type="text"
-          className="w-full h-full placeholder:text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5] border-gray-600 p-4 outline-none border-none"
-          placeholder="What's happening?" 
-        />
-        <div className="w-full justify-between items-center flex">
-          <div></div>
-          <div className="w-full max-w-[100px]">
-            <button className="rounded-full bg-primary py-2 px-4 text-lg text-center hover:bg-opacity-70 transition duration-200 font-bold">
-              Tweet
-            </button>
-          </div>
-        </div>
+        <div className="w-11 h-11 bg-slate-400 rounded-full flex-none"></div>
+        <ComposeTweet />
       </div>
-    </div>
     <div className="flex flex-col w-full">
     {Array.from({length: 5}).map((_,i) => (
       <div key={i} className="border-b-[0.5px] border-gray-600 p-4 flex space-x-4">
--- src/components/serverComponents/composeTweet.tsx
+++ src/components/serverComponents/composeTweet.tsx
@@ -1,51 +1,60 @@
-"use client";
+import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies} from "next/headers";
+import { randomUUID } from "crypto";
+import { SupabaseClient } from "@supabase/supabase-js";
 
-import { PostgrestError } from "@supabase/supabase-js";
-import React, { useRef } from "react";
-import { toast } from "sonner";
+import { revalidatePath } from "next/cache";
+import ComposeTweetForm from "../composeTweetForm";
+import { db } from "@/lib/db";
+import { tweets } from "@/lib/db/schema";
 
-type ComposeTweetFormProps = {
-  serverAction: any;
+const ComposeTweet = () => {
+  async function submitTweet(formData: FormData) {
+    "use server";
+
+    const tweet = formData.get("tweet");
+
+    if (!tweet) return;
+
+    const supabaseClient = createServerComponentClient({
+      cookies
+    });
+
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY;
+
+    if (!supabaseUrl || !supabaseSecretKey)
+      return { error: { message: "supabase credentials are not provided!" } };
+
+    const supabaseServer = new SupabaseClient(supabaseUrl, supabaseSecretKey);
+
+    const { data: userData, error: userError } =
+      await supabaseClient.auth.getUser();
+
+    if (userError) return;
+
+    let err = "";
+
+    const res = await db
+      .insert(tweets)
+      .values({
+        text: tweet.toString(),
+        id: randomUUID(),
+        profileId: userData.user.id,
+      })
+      .returning()
+      .catch((error) => {
+        console.log(error);
+        err = "something wrong with server";
+      });
+
+    revalidatePath("/");
+
+    return { data: res, error: err };
+  }
+
+  return <ComposeTweetForm serverAction={submitTweet} />;
 };
 
-const ComposeTweetForm = ({ serverAction }: ComposeTweetFormProps) => {
-  const resetRef = useRef<HTMLButtonElement>(null);
-
-  const handleSubmitTweet = async (data: any) => {
-    try {
-      const res = await serverAction(data);
-      if (res?.error) {
-        return toast.error(res.error.message);
-      }
-      toast.success("Tweet sent successfully!");
-      resetRef.current?.click();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <form action={handleSubmitTweet} className="flex flex-col w-full h-full">
-      <input
-        type="text"
-        name="tweet"
-        className="w-full h-full text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none"
-        placeholder="What's happening?"
-      />
-      <div className="w-full justify-between items-center flex">
-        <div></div>
-        <div className="w-full max-w-[100px]">
-          <button
-            type="submit"
-            className="rounded-full bg-twitterColor px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200 font-bold"
-          >
-            Tweet
-          </button>
-          <button ref={resetRef} className="invisible" type="reset"></button>
-        </div>
-      </div>
-    </form>
-  );
-};
-
-export default ComposeTweetForm;
+export default ComposeTweet;
